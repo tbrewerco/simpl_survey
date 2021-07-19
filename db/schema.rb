a55980eec83703f5172db0da26af3dc3860ea00d
cindytran <cindyztran@gmail.com>
2021-07-19 10:21:02 -0700

    adds survey question text to survey question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_170228) do
+ActiveRecord::Schema.define(version: 2021_07_19_172015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 2021_07_19_170228) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "categories"
+    t.text "survey_question_text"
   end
 
 end
