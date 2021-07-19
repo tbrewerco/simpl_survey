3c4d0b5605c999414972015a5532c3dd01b50e06
cindytran <cindyztran@gmail.com>
2021-07-19 12:18:17 -0700

    removes no longer needed migration files. seed data successfully running

--- db/migrate/20210719163039_remove_answer_boolean_from_answer_options.rb
+++
@@ -1,5 +0,0 @@
-class RemoveAnswerBooleanFromAnswerOptions < ActiveRecord::Migration[6.1]
-  def up
-    remove_column :answer_options, :answer_boolean, :boolean
-  end
-end
--- db/migrate/20210719170228_add_categories_to_survey_questions.rb
+++
@@ -1,5 +0,0 @@
-class AddCategoriesToSurveyQuestions < ActiveRecord::Migration[6.1]
-  def change
-    add_column :survey_questions, :categories, :string
-  end
-end
