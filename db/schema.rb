f01dd7222d4c98918d68bc441d2c6e057edac798
cindytran <cindyztran@gmail.com>
2021-07-20 07:58:30 -0700

    updates schema for convention

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_20_143853) do
+ActiveRecord::Schema.define(version: 2021_07_20_145502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2021_07_20_143853) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "survey_question_id"
-    t.integer "answer_options_id"
+    t.integer "answer_option_id"
   end
 
   create_table "survey_questions", force: :cascade do |t|
